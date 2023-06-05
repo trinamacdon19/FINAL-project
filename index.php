0bc882b7390b6c7aca107048bce3113b44ed92ce
Trina Macdon <120351433+trinamacdon19@users.noreply.github.com>
2023-06-05 10:11:52 -1200

    Update index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html> 
+<html> 
+<head>
+	<meta name="referrer" content="origin-when-crossorigin" id="meta_referrer"> 	
+	<title>Tourism Website</title> 
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+	<link rel="preconnect" href="https://fonts.googleapis.com"> 
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
+	<link rel="preconnect" href="https://fonts.googleapis.com"> 
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+	<link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet"> 
+
+
+</head> 
+<body>
+	<div class="navbar"> 
+		<h1>BUENA VISITA</h1> 
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li> 
+				<li><a href="tours.php">Tours</a></li> 
+				<li><a href="about.php">About</a></li> 
+				<li><a href="contact.php">Contact</a></li> 
+			</ul>
+			</nav> 
+	</div> 
+	<div class="body"> 
+		<div class="heading"> 
+			<h1>Explore the Wonders of Marinduque</h1> 
+			<br> 
+			<p>Tara na</p> 
+			<br> 
+			<br> 
+	 
+		</div> 
+		<div class="tours"> 
+			<div class="places"> 
+				<img src="image/buenavista.jpg" style = "width: 300px; height: 250px; border-radius: 12px;"> 
+				 <br> 
+				 <br> 
+			</div> 
+			<div class="places"> 
+				<img src="image/municipal.jpeg" style = "width: 300px; height: 250px; 
+				border-radius: 12px;"> 
+				 <br>
+				 <br> 
+			</div> 
+		     <div class="places"> 
+				<img src="image/mt.malindig.jpeg" style = "width: 300px; height: 250px; border-radius: 12px;"> 
+				 <br> 
+				 <br> 
+			</div> 
+		</div> 
+	</div> 
+
+</body> 
+</html>
